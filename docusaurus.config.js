f700da2929c3d5b918ffb327bf01fda9d194a9b5
Almog Gavra <almog.gavra@gmail.com>
2023-08-23 16:25:33 -0700

    add google analytics (#5)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
     [
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           routeBasePath: '/',
           sidebarCollapsed: false,
@@ -50,7 +50,11 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+        gtag: {
+          trackingID: 'G-9GJEEDV70G',
+          anonymizeIP: true,
+        },
+      },
     ],
   ],
 
