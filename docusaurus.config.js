02d9024f7294d03591a0da918799f41936227e1c
Apurva Mehta <830220+apurvam@users.noreply.github.com>
2023-09-13 12:22:28 -0700

    make the 'Our' lowercase

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
           },
           {
             to: 'https://responsive.dev/sign-up-for-responsive-beta',
-            label: 'Join Our Waitlist',
+            label: 'Join our Waitlist',
             position: 'right',
             className: 'button button--secondary button--rg'
           },
