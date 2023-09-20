5fa767d8740821c32efd022ad865c87588e44d89
Apurva Mehta <830220+apurvam@users.noreply.github.com>
2023-09-19 21:45:15 -0700

    fix image url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/open-graph-img.png',
+      image: 'img/open-graph-image.png',
       navbar: {
         logo: {
           alt: 'Responsive Logo',
