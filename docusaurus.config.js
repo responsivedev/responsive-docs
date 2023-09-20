4770e7fdf76951819eda4ae8a806d3f19a478cf5
Apurva Mehta <830220+apurvam@users.noreply.github.com>
2023-09-19 21:34:13 -0700

    Add opengraph image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/logo-emblem.svg',
+      image: 'img/open-graph-img.png',
       navbar: {
         logo: {
           alt: 'Responsive Logo',
